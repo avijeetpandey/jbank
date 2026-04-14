5c57d0463f47646386c2e596ebf12f4c7a18ebd7
Avijeet Pandey <avijeetpandey25@gmail.com>
2026-04-14 15:25:58 +0530

    test: simplify baseline application test

--- src/test/java/com/avijeet/jbank/JbankApplicationTests.java
+++ src/test/java/com/avijeet/jbank/JbankApplicationTests.java
@@ -1,13 +1,7 @@
 package com.avijeet.jbank;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-        "spring.autoconfigure.exclude="
-                + "org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration,"
-                + "org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration"
-})
 class JbankApplicationTests {
 
     @Test
